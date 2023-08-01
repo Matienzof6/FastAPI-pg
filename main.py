4f330ae5367a9ca2678ae8662c0adb6222a03734
Matias Pereiro <matienzof6@gmail.com>
2023-08-01 21:32:05 +0200

    Esquema, write y la funcion instert

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from models.user_connection import UserConnection
-
+from schema.user_schema import UserSchema
 app = FastAPI()
 conn = UserConnection()
 
@@ -8,3 +8,10 @@ conn = UserConnection()
 def root():
     conn
     return "Hi Im FastAPI"
+
+
+@app.post("/api/insert")
+def insert(user_data:UserSchema): #Con esta funcion usamos el UserSchema para que se puedan pasar solo los datos indicados en la clase UserSchema 
+    data = user_data.dict() #Listamos los datos en un diccionaro
+    data.pop("id") #Pasamos por alto el id
+    conn.write(data) #Introducimos los valores dentro de la base de datos mediante la funcion write creada anteriormente
--- models/user_connection.py
+++ models/user_connection.py
@@ -11,6 +11,16 @@ class UserConnection():
             print(err)
             self.conn.close() #En el caso de que haya algun error al conectar, se cierra la base de datos
 
+    def write(self, data):
+        with self.conn.cursor() as cur: #Con el with creamos un contexto en el cual va a estar funcionando la base de datos, el cual terminara la conexion al dejar de usarlo, para eso se usa el contexto
+            cur.execute("""
+                INSERT INTO "user"(name, phone) VALUES(%(name)s, %(phone)s)
+            """, data) # Con estas lineas le decimos a nuesrta base de datos que queremos insertar datos en los camos name y phone de user con los valores que le pasamos mediante %()s el diccionario que contiene el parametro data 
+        self.conn.commit()
+
+
+
+
 
     def __def__(self):  #Luego de ejecutar cualquier cosa dentro de la clase queremos cerrar la conexion a la base de datos
         self.conn.close()
